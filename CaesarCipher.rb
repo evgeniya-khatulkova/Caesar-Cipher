2277f1e37d4aa1ff2215f70279894f75ae0833b5
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-27 00:38:33 +0200

    solved

--- CaesarCipher.rb
+++ CaesarCipher.rb
@@ -5,6 +5,8 @@ def caesar(word, shift)
       letter = letter.ord + shift
        if (65..90).include?(letter - shift) && letter > 90
          letter = letter - 91 + 65
+       elsif (97..122).include?(letter - shift) && letter > 122
+        letter = letter - 123 + 97
        end
     end
       letter.chr
