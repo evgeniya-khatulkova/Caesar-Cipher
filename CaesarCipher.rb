4cf2dddd9319c72a680de3a96cac78c1b42a4bab
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-26 23:49:38 +0200

    incrypt only letters(both case) ommit other symbols

--- CaesarCipher.rb
+++ CaesarCipher.rb
@@ -1,7 +1,10 @@
-def caesar(letter, schift)
-    result = word.split("")
-    result = result.map do |letter|
+def caesar(word, shift)
+  result = word.split("")
+  result = result.map do |letter|
+    if (65..90).include?(letter.ord) || (97..122).include?(letter.ord)
       letter = letter.ord + shift
-      letter.chr
     end
+      letter.chr
+  end
+  result.join
 end
