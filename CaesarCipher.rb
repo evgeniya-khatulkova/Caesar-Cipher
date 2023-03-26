535bd43c478c106da508930280c189553e09d85d
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-27 00:26:39 +0200

    solve for letters upcase

--- CaesarCipher.rb
+++ CaesarCipher.rb
@@ -3,6 +3,9 @@ def caesar(word, shift)
   result = result.map do |letter|
     if (65..90).include?(letter.ord) || (97..122).include?(letter.ord)
       letter = letter.ord + shift
+       if (65..90).include?(letter - shift) && letter > 90
+         letter = letter - 91 + 65
+       end
     end
       letter.chr
   end
